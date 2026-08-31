ecc7b7dd8dbd47b5b76072911412f90706905971
holic512 <holic512@163.com>
2026-08-31 14:40:50 +0800

    [feat] [1.新增基础业务与项目业务模块骨架。 2.更新服务器模块组装依赖。]

---
+++ pipker-business/src/main/java/com/pipker/business/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * @file package-info.java
+ * @project Pipker Framework
+ * @module Pipker Business
+ * @description Root package for future application-specific business capabilities.
+ * @logic Establishes the module package boundary without defining project business implementations.
+ * @dependencies None
+ * @index_tags package,project-business,module-boundary
+ * @author holic512
+ */
+package com.pipker.business;
